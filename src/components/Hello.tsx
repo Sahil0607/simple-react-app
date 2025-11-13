16485cb50abb40bb37d651c0019b274e1d24624e
sahilpatel <patel.sahil331@gmail.com>
2025-11-13 14:55:52 -0600

    Hello tsx file added

---
+++ src/components/Hello.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useState } from "react";
+import ItemList from "./ItemList/ItemList";
+
+const Hello = ({ name = "World" }) => {
+  const [itemName] = useState("item component");
+
+  return (
+    <div className="hello">
+      <h2>Hello, {name}!</h2>
+      <ItemList name={itemName} />
+    </div>
+  );
+};
+
+export default Hello;
